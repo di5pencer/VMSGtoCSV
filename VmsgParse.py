44ae18247aaf0e734f02423f7128e48112311aef
sb8887 <30408514+sb8887@users.noreply.github.com>
2019-11-01 18:31:12 +1030

    Fixed errors.

--- VmsgParse.py
+++ VmsgParse.py
@@ -17,42 +17,50 @@ hexBuffer = []
 # Make some functions.
 
 #converts the list array of characters from the buffer to a string
+
+
 def stringConvert(s):
     new = ""
     # traverse in the string
     for b in s:
         new += b
     # return string
-    return new
+    return new.strip('\n')
 
 #converts the hex characters into ASCII
 
+
 def hexConvert(s1):
-    pos=0
+    pos = 0
+    sms = ""
     for a1 in s1:
         #count some letters so we can loop
         length = len(s1)
         if pos <= length:
             try:
                 #print(bytearray.fromhex(a1).decode())
-                hexBuffer.append(bytearray.fromhex(a1).decode())
-                pos=pos+1
+                hexBuffer.append(bytearray.fromhex(
+                    a1.strip('\n')).decode('unicode_escape'))
+                pos = pos+1
             #needs more robust error correction
-            except ValueError:
-                pass
+            except ValueError as e:
+                print("error")
+                print(e)
+                #pass
 
 #once it has reached the end of the input characters, convert to a string, empty list etc.
         if pos >= length:
             sms = stringConvert(hexBuffer)
             print("Succesfully decoded SMS: " + str(sms))
-            listConvert.append(sms)
             hexBuffer.clear()
-            pos=0
+            pos = 0
+            return sms
 
 #read the file and write data to lists.
 
+
 print("Loading data. \n")
-with open (file) as f:
+with open(file) as f:
     for x in f:
         if x.startswith('Date:'):
             x = x.split(':')
@@ -64,24 +72,24 @@ with open (file) as f:
             x = x.split(':')
             x = x[1]
             listSMShex.append(x.strip('\n'))
+            message = x.split('=')
+            message1 = hexConvert(message)
+            listConvert.append(message1)
 
         if x.startswith('X-READ:'):
             x = x.split(':')
             x = x[1]
             listStatus.append(x.strip('\n'))
 
-for z in listSMShex:
-    z = z.split("=")
-    sms=hexConvert(z)
-      
+
 ## Zip the lists together
-rows = zip(listDate,listStatus,listSMShex,listConvert)
+rows = zip(listDate, listStatus, listSMShex, listConvert)
 
 #Write out a CSV
 print("\nWriting CSV.")
-with open('csv.csv', "w") as f:
+with open('csv.csv', "w", newline='', encoding='utf-8') as f:
     writer = csv.writer(f)
-    writer.writerow(["Date: ",'Status: ', "HEX: ","Converted: "])
+    writer.writerow(["Date: ", 'Status: ', "HEX: ", "Converted: "])
     for row in rows:
         writer.writerow(row)
 print("Converion complete, exiting.")
